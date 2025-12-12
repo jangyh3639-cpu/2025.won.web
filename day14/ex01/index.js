05002a4e75fa32916ff36c046409ffa80a05d437
jangyh3639 <jangyh3639@gmail.com>
2025-12-12 10:58:32 +0900

    기말연습

---
+++ day14/ex01/index.js
@@ -0,0 +1,16 @@
+export default async function main() {
+
+
+    const inpMsg = document.querySelector('#inp-msg')
+    const btnSend = document.querySelector('#btn-send')
+    const txtResponse = document.querySelector('#response')
+
+    btnSend.addEventListener(()=>{
+        console.log('msg : ')
+    })
+
+
+
+
+    console.log("hello es6");
+}
